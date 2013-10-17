c0bcece5e700c57081b30a9164dfb69fccd0222d
Steffen Tröster <stetro.freaky@googlemail.com>
2013-10-17 17:52:21 +0300

    Leaflet map integration (beginning)

--- phone/app/scripts/app.js
+++ phone/app/scripts/app.js
@@ -8,7 +8,7 @@ var app = {
 		document.addEventListener('deviceready', this.onDeviceReady, true);
 	},
 	onDeviceReady: function() {
-		console.log("REPORT - Device Ready!");
+		console.log('REPORT - Device Ready!');
 		angular.element(document).ready(function() {
 			angular.bootstrap(document);
 		});
@@ -17,15 +17,21 @@ var app = {
 
 app.initialize();
 
-angular.module('phoneApp', [])
-	.config(function($routeProvider, $compileProvider) {
-		$routeProvider
-			.when('/', {
-				templateUrl: 'views/main.html',
-				controller: 'MainCtrl'
-			})
-			.otherwise({
-				redirectTo: '/'
-			});
-		$compileProvider.urlSanitizationWhitelist(/^\s*(https?|ftp|mailto|file|tel):/);
-	});
+var phoneApp = angular.module('phoneApp', ["leaflet-directive"]);
+
+phoneApp.config(function($routeProvider, $compileProvider) {
+	$routeProvider
+		.when('/', {
+			templateUrl: 'views/main.html',
+			controller: 'MainCtrl'
+		})
+		.when('/map', {
+			templateUrl: 'views/map.html',
+			controller: 'MapCtrl'
+		})
+		.otherwise({
+			redirectTo: '/'
+		});
+	$compileProvider.urlSanitizationWhitelist(/^\s*(https?|ftp|mailto|file|tel):/);
+});
+
---
+++ phone/app/scripts/controllers/Map.js
@@ -0,0 +1,15 @@
+'use strict';
+
+angular.module('phoneApp')
+	.controller('MapCtrl', function($scope) {
+		$scope.center = {
+			lat: 61.4981508,
+			lng: 23.7610254,
+			zoom: 13
+		};
+		angular.extend($scope, {
+			defaults: {
+				scrollWheelZoom: false
+			}
+		});
+	});
