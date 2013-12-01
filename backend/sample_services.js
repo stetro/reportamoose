fa64b20728f1f1982266a1a5311e3a0b7f523729
Steffen Tröster <stetro.freaky@googlemail.com>
2013-12-01 16:39:50 +0200

    remove everything before uploading new stuff

--- backend/sample_services.js
+++ backend/sample_services.js
@@ -93,72 +93,78 @@ var items = [{
     "keywords": "some keywords",
     "group": "road",
     "metadata": true
-},
-
-
-
-
- {
+}, {
     "service_name": "Rikkinäinen tai puuttuva katuvalo",
     "description": "Rikkinäinen tai puuttuva katuvalo",
     "type": "realtime",
     "keywords": "some keywords",
     "group": "light",
     "metadata": true
-},
- {
+}, {
     "service_name": "Rikkinäinen tai puuttuva liikennemerkki",
     "description": "Rikkinäinen tai puuttuva liikennemerkki",
     "type": "realtime",
     "keywords": "some keywords",
     "group": "light",
     "metadata": true
-},
- {
+}, {
     "service_name": "Bussipysäkin puute tai vika",
     "description": "Bussipysäkin puute tai vika",
     "type": "realtime",
     "keywords": "some keywords",
     "group": "light",
     "metadata": true
-},
- {
+}, {
     "service_name": "Puute urakoitsijan toiminnassa",
     "description": "Puute urakoitsijan toiminnassa",
     "type": "realtime",
     "keywords": "some keywords",
     "group": "light",
     "metadata": true
-},
- {
+}, {
     "service_name": "Levähdysalueen tai parkkipaikan vika tai puute",
     "description": "Levähdysalueen tai parkkipaikan vika tai puute",
     "type": "realtime",
     "keywords": "some keywords",
     "group": "light",
     "metadata": true
-},
- {
+}, {
     "service_name": "Tunnelin vika tai puute",
     "description": "Tunnelin vika tai puute",
     "type": "realtime",
     "keywords": "some keywords",
     "group": "light",
     "metadata": true
-},
- {
+}, {
     "service_name": "Muu",
     "description": "Muu",
     "type": "realtime",
     "keywords": "some keywords",
     "group": "light",
     "metadata": true
-}
+}];
+
+console.log('deleting all available services ...')
+request.get({
+    url: service_url + 'services',
+    json: true
+}, function(error, res, body) {
+    if (!error && res.statusCode == 200) {
+        for (i in body) {
+            request.del({
+                url: service_url + 'services/' + body[i]._id,
+                json: true
+            }, function(error, res, body) {
+                process.stdout.write(".");
+            });
+        }
+    } else {
+        console.log(res.body);
+    }
+});
 
 
-
-
-];
+process.stdout.write("Saving New: ");
 
 for (i in items) {
     request.post({
@@ -173,5 +179,3 @@ for (i in items) {
         }
     });
 }
-
-process.stdout.write("Saving: ");
